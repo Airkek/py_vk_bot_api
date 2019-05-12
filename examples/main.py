c02de85b89cb303cc6bcd7ebbb13adf3847894e1
Taho <antonyblack05@gmail.com>
2019-05-12 23:55:09 +0600

    added all upload methods

--- examples/main.py
+++ examples/main.py
@@ -14,8 +14,17 @@ res = api.call("users.get", {'user_ids': 1})
 Загрузка документов на сервер VK
 """
 upload = lib.upload(session)
-res = upload.audioMessage('audio.mp3') #загрузка аудиосообщений
-"Comming Soon..."
+
+
+res = upload.messageDocument(filename='file.zip', peer_id=1) #загрузка документов для отправки в сообщениях
+res = upload.audioMessage(filename='audio.mp3', peer_id=1) #загрузка аудиосообщений
+res = upload.messagePhoto(filename='photo.png', peer_id=1) #загрузка фото для отправки в сообщениях
+
+"Следующие методы можно вызвать только с пользовательским ключем доступа"
+res = upload.albumPhoto(filename='photo.png', album_id=1, group_id=1) #загрузка фото в альбом
+res = upload.wallPhoto(filename='photo.png', group_id=1, caption="Моё крутое фото") #загрузка фото для стены
+res = upload.document(group_id=1) #загрузка документов
+
 
 """
 Создание бота (группа)
@@ -36,5 +45,5 @@ bot.startPolling() #начинаем получать события с LongPoll
 
 """
 Этого сделать больше нельзя (можно только если через токен Kate Mobile (получается через lib.userAuth))
-Вскоре даже черех Kate Mobile нельзя будет, Users LongPoll делать смысла я не вижу
+Вскоре даже через Kate Mobile нельзя будет, Users LongPoll делать смысла я не вижу
 """
--- py_vk_bot_api/upload.py
+++ py_vk_bot_api/upload.py
@@ -9,11 +9,72 @@ class upload(object):
             raise mySword("invalid session")
         self.vk = api(session).call
 
-    def audioMessage(self, filename, peer_id):
-        url = self.vk("docs.getMessagesUploadServer", {'peer_id': peer_id, 'type': 'audio_message'})['upload_url']
+    def messageDocument(self, filename, peer_id, t='doc'):
+        url = self.vk("docs.getMessagesUploadServer", {
+        'peer_id': peer_id,
+        'type': t
+        })
+        url = url['upload_url'] if not "error" in url else raise mySword(url['error_msg'])
+
         files = [('file', (filename, open(filename, 'rb')))]
         res = post(url, files=files).json()['file']
-        saved = self.vk('docs.save', {'file': res})
-        if "audio_message" in saved:
-            return saved['audio_message']
-        return saved
+
+        return self.vk('docs.save', {'file': res})
+
+    def audioMessage(self, filename, peer_id):
+        return self.messageDocument(filename, peer_id, 'audio_message')
+
+    def document(self, group_id=None):
+        url = self.vk("docs.getMessagesUploadServer", {'group_id': group_id})
+        url = url['upload_url'] if not "error" in url else raise mySword(url['error_msg'])
+
+        files = [('file', (filename, open(filename, 'rb')))]
+        res = post(url, files=files).json()['file']
+
+        return self.vk('docs.save', {'file': res})
+
+    def messagePhoto(self, filename, peer_id):
+        url = self.vk("docs.getMessagesUploadServer", {'peer_id': peer_id})
+        url = url['upload_url'] if not "error" in url else raise mySword(url['error_msg'])
+
+        files = [('file', (filename, open(filename, 'rb')))]
+        res = post(url, files=files).json()['file']
+
+        return self.vk("photos.saveMessagesPhoto", {
+        'photo': res['photo'],
+        'server': res['server'],
+        'hash': res['hash']
+        })
+
+    def albumPhoto(self, filename, album_id, group_id=None):
+        res = self.vk("photos.getUploadServer", {
+        'group_id': group_id,
+        'album_id': album_id
+        })
+        url = res['upload_url'] if not "error" in res else raise mySword(url['error_msg'])
+
+        files = [('file', (filename, open(filename, 'rb')))]
+        res = post(url, files=files).json()
+
+        return self.vk('photos.save', {
+        'album_id': album_id,
+        'group_id': group_id,
+        'server': res['server'],
+        'photos_list': res['photos_list'],
+        'hash': res['hash']
+        })
+
+    def wallPhoto(self, filename, group_id=None, caption=None):
+        res = self.vk("photos.getUploadServer", {'group_id': group_id})
+        url = res['upload_url'] if not "error" in res else raise mySword(url['error_msg'])
+
+        files = [('file', (filename, open(filename, 'rb')))]
+        upl = post(url, files=files).json()
+
+        return self.vk('photos.saveWallPhoto', {
+        'group_id': group_id,
+        'photo': upl['photo'],
+        'server': upl['server'],
+        'hash': upl['hash'],
+        'caption': caption
+        })
