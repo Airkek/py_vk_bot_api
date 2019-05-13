13d30b26e0d80443844970b04dd0cd713342d074
Taho <antonyblack05@gmail.com>
2019-05-13 23:32:51 +0600

    update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 from pipenv.project import Project
 from pipenv.utils import convert_deps_to_pip
 
@@ -16,8 +13,6 @@ setuptools.setup(
     author='Airkek',
     author_email='antonyblack05@gmail.com',
     description='Python vk.com API wrapper',
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     url='https://github.com/Airkek/py_vk_bot_api/',
     packages=setuptools.find_packages(),
     classifiers=[
