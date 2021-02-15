8f2bd674d06aaa32db2d4af39453966bb35bc4a9
Keijia <antonyblack05@gmail.com>
2021-02-15 15:11:24 +0300

    asdd

--- setup.py
+++ setup.py
@@ -1,24 +1,18 @@
-import setuptools
+from distutils.core import setup
 
-from pipenv.project import Project
-from pipenv.utils import convert_deps_to_pip
-
-def packages():
-    pipfile = Project(chdir=False).parsed_pipfile
-    return convert_deps_to_pip(pipfile['packages'], r=False)
-
-setuptools.setup(
+setup(
     name='py_vk_bot_api',
-    version='1.1',
+    version='1.2',
     author='Airkek',
     author_email='',
     description='Python vk.com API wrapper',
     url='https://github.com/Airkek/py_vk_bot_api/',
-    packages=setuptools.find_packages(),
+    packages=['py_vk_bot_api'],
     classifiers=[
         "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    install_requires=packages()
+    license='MIT',
+    install_requires=['requests']
 )
